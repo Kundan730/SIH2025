b41b7029b25e274dc0b1af073901e4fed5f8cb33
PritamP20 <pripritam7@gmail.com>
2025-09-15 12:14:01 +0530

    changes

--- Blockchain/src/CarbonToken.sol
+++ Blockchain/src/CarbonToken.sol
@@ -2,21 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
-
-/// @title Carbon Credit Token
 contract CarbonToken is ERC20 {
-    address public registry; // only registry can mint
+    address public registry; 
 
-    constructor() ERC20("Carbon Credit Token", "CCT") {
-        registry = msg.sender; // deployer will be registry contract
+    constructor() ERC20("Blue Carbon Token", "BCT") {
+        registry = msg.sender; 
     }
 
     modifier onlyRegistry() {
         require(msg.sender == registry, "Not authorized");
         _;
     }
-
-    /// @notice Mint new tokens (only registry)
     function mint(address to, uint256 amount) external onlyRegistry {
         _mint(to, amount);
     }
